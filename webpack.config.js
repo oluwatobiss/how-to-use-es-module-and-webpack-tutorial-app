8d9a43facd4eb9c22611e3efed1cfd3f4a7e1fdc
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2022-05-14 00:50:26 +0100

    Install html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = { 
+    plugins: [new HtmlWebpackPlugin()] ,
     devServer: {
+        static: "./dist",
         port: 3000
     }
 }
